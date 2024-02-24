ddc49829a96b7cc50b30ba5deb72b3e8b5a83304
mahsima <mahsimaeb83@gmail.com>
2024-02-24 19:39:28 +0330

    sfdds

--- DatabasePys/FormsMethodCar.py
+++ DatabasePys/FormsMethodCar.py
@@ -11,21 +11,21 @@ class FormsMethodCarclass:
         @wraps(func)
         def NullChekerWrap(self,TheModelInstance,TheClassObj):
             culomnLst=TheModelInstance.__table__.columns._all_columns
-            NullErorr="فیلد های زیر خالی است "
-            LengthError="فیلد های زیر بیشتر از حد مجاز اند "
+            NullErorr=" :فیلد های زیر خالی است "
+            LengthError=" :فیلد های زیر بیشتر از حد مجاز اند"
             runstate=True
             for i in culomnLst:
                 if (i.type.python_type==str):
                     if( i.autoincrement=='auto'and i.type.length is not None):
                             if(i.type.length<len(TheModelInstance.__dict__[i.name])):
-                                LengthError+=i.description
+                                LengthError=i.description
                                 runstate=False
                             if (i.nullable==False) :
                                     if (str(TheModelInstance.__dict__[i.name]).strip()==""):
                                         NullErorr+=i.description
                                         runstate=False
 
-            resDecorator=NullErorr+LengthError
+            resDecorator=NullErorr+"\n"+LengthError
 
             if runstate:
                     func(self,TheModelInstance,TheClassObj)
